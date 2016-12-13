dd71a0ce59b7517619af9e95b3792e3aaf8c7ccb
AliJP <a.jaberipour@gmail.com>
2016-12-13 12:43:20 +0330

    Update FlickrPhoto.js

--- Scripts/FlickrPhoto.js
+++ Scripts/FlickrPhoto.js
@@ -6,19 +6,17 @@
 
     function getPhotoElement(photo) {
         var photoNode = document.createElement("div");
-        photoNode.classList = ["photo-item search-card"];
-        photoNode.addEventListener("click", selectPhoto);
+        photoNode.classList.add("photo-item");
+        photoNode.classList.add("card-item");
         photoNode.setAttribute("data-photo-id", photo.id);
         var photoImage = document.createElement("img");
         photoImage.src = Services.getPhotoThumpnail(photo);
         photoImage.alt = photo.title;
-        photoImage.classList = ["photo-image"];
+        photoImage.classList.add("photo-image");
         photoNode.appendChild(photoImage);
         var photoTitle = document.createElement("p");
-        photoTitle.classList = ["photo-title"];
+        photoTitle.classList.add("photo-title");
         photoTitle.innerHTML = photo.title;
-        //var photoTitleInnerText = document.createTextNode(photo.title);
-        //photoTitle.appendChild(photoTitleInnerText);
         photoNode.appendChild(photoTitle);
 
         return photoNode;
@@ -38,6 +36,7 @@
             for (var i = 0 ; i < photos.photo.length; i++) {
                 dsPhotos.push(photos.photo[i]);
                 var newPhoto = getPhotoElement(photos.photo[i])
+                newPhoto.addEventListener("click", selectPhoto);
                 searchResult.appendChild(newPhoto);
             }
             if ((pageIndex) * pageSize < photos.total)
@@ -47,29 +46,34 @@
         }
     }
 
-    function getPhotos(searchQuery) {
+    function getPhotos(searchQuery, callbackFunction) {
         document.getElementsByClassName("search-loading")[0].style.display = "block";
-        Services.getPhotos(searchQuery, pageSize, pageIndex, addPhotos);
+        Services.getPhotos(searchQuery, pageSize, pageIndex, callbackFunction
+            , function () {
+            document.getElementsByClassName("search-loading")[0].style.display = "none";
+        });
     }
 
     function loadPhotos(searchQuery) {
         pageIndex = 1;
-        pageSize = Number.parseInt(document.getElementsByClassName("search-result")[0].offsetWidth / 320) * 2;
+        pageSize = parseInt(document.getElementsByClassName("search-result")[0].offsetWidth / 320) * 2;
         var searchResult = document.getElementsByClassName("search-result")[0];
-        searchResult.innerHTML = "";
 
-        getPhotos(searchQuery);
+        getPhotos(searchQuery, function (photos) {
+            searchResult.innerHTML = "";
+            addPhotos(photos);
+        });
     }
 
     function loadMorePhotos(searchQuery) {
         pageIndex++;
-        getPhotos(searchQuery);
+        getPhotos(searchQuery, addPhotos);
     }
 
     function selectPhoto(event) {
         var selectedElement = event.currentTarget;
         var selectedID = selectedElement.getAttribute("data-photo-id");
-        let selectedPhoto = Photos.DataSource.find(function (photo) { return photo.id == selectedID });
+        var selectedPhoto = Photos.DataSource.find(function (photo) { return photo.id == selectedID });
         if (selectedPhoto.selected) {
             selectedPhoto.selected = false;
             selectedElement.classList.remove("selected-item")
@@ -78,12 +82,26 @@
             selectedElement.classList.add("selected-item")
             selectedPhoto.selected = true;
         }
+
+        var selectedLength = Photos.DataSource.filter(function (photo) { return photo.selected == true }).length;
+
+        var viewGalleryElement = document.getElementsByClassName("show-gallery")[0];
+        if (selectedLength > 0)
+            viewGalleryElement.removeAttribute("disabled");
+        else
+            viewGalleryElement.setAttribute("disabled", "");
+    }
+
+    function showGallery() {
+        JAPGallery.DataSource = Photos.DataSource.filter(function (photo) { return photo.selected == true });
+        JAPGallery.openGallery();
     }
 
     window.Photos = Utility.extend(window.Photos || {}, {
         getPhotoElement: getPhotoElement
         , loadPhotos: loadPhotos
         , loadMorePhotos: loadMorePhotos
+        , showGallery: showGallery
         , DataSource: dsPhotos
     });
 })(document, window);
